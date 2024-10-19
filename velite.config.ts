8aaab6f3943acc3db262b9c0d94d147b415d4fd9
gabrieldnsilva <gabrieldnsilva@gmail.com>
2024-10-19 16:28:43 -0300

    next-app setup

---
+++ velite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'velite'
+import { PluggableList } from 'unified'
+
+const config = defineConfig({
+  collections: {},
+  mdx: {}
+})
+
+export default config
---
+++ website.config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    metadata: {
+      title: "Gabriel Danilo Portfolio's",
+      description:
+        'Passionate with technology, this portfolio showcases my work and experiences.',
+    },
+    webserver: {
+      host: process.env.HOST ?? 'http://localhost:3000'
+    }
+  }
